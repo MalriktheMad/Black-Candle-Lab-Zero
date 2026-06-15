2fedd0cdc37018f434872cc234685c3277fc881c
Malrik The Mad <VorpalGnome@gmail.com>
2026-06-14 21:06:04 -0700

    Update majira.js

--- js/dialogues/majira.js
+++ js/dialogues/majira.js
@@ -1,5 +1,6 @@
 function getMajiraDialogueLines() {
   return [
-    majiraLine("Hello, Little Wing.")
+    majiraLine("Little Wing! You broke out of your cage again! Well I am glad you came to me. I would be really worried if you were gone when I got back."),
+    littleWingLine("Don't worry, I am just looking for snacks. I'll head home soon!")
   ];
 }
